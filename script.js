432f0a78b3fa00ec81e20c5ce00391924abafde8
Niels-Maker <nielsbb2008@gmail.com>
2025-04-17 09:12:09 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+    const navLinks = document.querySelectorAll('nav a');
+    navLinks.forEach(link => {
+        link.addEventListener('mouseover', () => {
+            link.style.color = '#d2691e';
+        });
+        link.addEventListener('mouseout', () => {
+            link.style.color = '#8b4513';
+        });
+    });
+});
